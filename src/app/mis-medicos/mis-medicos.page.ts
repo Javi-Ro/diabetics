2a3ce8086d6fc025c6d8a89ad07c66f3fd198221
Javier Rodríguez <jrj10@alu.ua.es>
2022-06-10 23:49:08 +0200

    Mis-medicos section nearly finished

--- src/app/mis-medicos/mis-medicos.page.ts
+++ src/app/mis-medicos/mis-medicos.page.ts
@@ -9,24 +9,51 @@ import { map } from 'rxjs/operators';
 })
 export class MisMedicosPage implements OnInit {
 
+  medicosVisibles: any = [];
   medicos: any = [];
+  medicosBuscar: any = [];
+  showSearch: boolean = false;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.getMedicos().subscribe(res => {
+      this.medicosVisibles = res;
       this.medicos = res;
-    })
+    });
+    this.getMedicosBuscar().subscribe(res => {
+      this.medicosBuscar = res;
+    });
   }
 
   getMedicos() {
+    return this.http
+    .get("assets/files/mis-medicos.json")
+    .pipe(
+      map((res:any) => {
+        return res.medicos
+      })
+    );
+  }
+
+  getMedicosBuscar() {
     return this.http
     .get("assets/files/medicos.json")
     .pipe(
       map((res:any) => {
         return res.medicos
       })
-    )
+    );
+  }
+
+  toggleSearchView() {
+    if(!this.showSearch) {
+      this.medicosVisibles = this.medicosBuscar;
+    }
+    else{
+      this.medicosVisibles = this.medicos;
+    }
+    this.showSearch = !this.showSearch;
   }
 
 }
